9d08f7350bf06a180e20e3ca09b27db5850fe968
Sebastian Zdziebko <zdziebko.sebastian17@gmail.com>
2025-04-08 20:23:59 +0200

    Added camera movement

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include "raymath.h"
 
 int main(){
     // Create new window for game
@@ -7,9 +8,27 @@ int main(){
     Texture2D backgroundMap = LoadTexture("nature_tileset/OpenWorldMap.png");
     Vector2 backgroundPos{0.0, 0.0};
 
+    // Set movement speed
+    const float speed{4.0};
+
+    SetTargetFPS(60);
+
     // Game loop
     while(!WindowShouldClose()){
         BeginDrawing();
+        ClearBackground(WHITE);
+
+        // Movement
+        Vector2 direction{};
+        if(IsKeyDown(KEY_A)) direction.x -= 1.0;
+        if(IsKeyDown(KEY_D)) direction.x += 1.0;
+        if(IsKeyDown(KEY_W)) direction.y -= 1.0;
+        if(IsKeyDown(KEY_S)) direction.y += 1.0;
+        if(Vector2Length(direction) != 0.0){
+            // Set backgroundPos = backgroundPos - direction
+            backgroundPos = Vector2Subtract(backgroundPos, Vector2Scale(Vector2Normalize(direction), speed));
+        }
+
         DrawTextureEx(backgroundMap, backgroundPos, 0.0, 4.0, WHITE);
 
         EndDrawing();
